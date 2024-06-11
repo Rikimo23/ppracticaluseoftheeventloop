e9942f7762468cf3d9ea447db8454562ac44dce0
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-06-11 12:41:27 -0400

    just commiting

---
+++ java.js
@@ -0,0 +1,12 @@
+/*Declare a global counter variable.
+Create a simple function that increments the variable, and then calls itself recursively.
+Surround the initial function call in a try/catch block.
+Within the catch, log the error and the value of the counter variable.*/
+
+let counter = 0;
+
+try{
+    function increment(){
+    counter++;
+    return increment()
+}
